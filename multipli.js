16b5309416ff30734e84aa8b6d6e4328af140bb7
zakiasegond <segondzakia@live.fr>
2018-06-26 15:35:52 +0200

    tableau vide

--- multipli.js
+++ multipli.js
@@ -1,9 +1,26 @@
-console.log("hello");
-var tab = new Array ();
+
+
+var tab = [' ',1,2,3,4,5,6,7,8,9];
+var tab1 = [1,2,3,4,5,6,7,8,9];
+var div = $("#tableau");
 
 
 
+$("#tableau").append("<table border='2px'>");
 
+	$("#tableau").append("<tr>");
+
+	for(var i = 0; i < tab.length; i++) {
+		$("#tableau").append("<td>"+tab[i]+"</td>");
+	}
+	for (var i = 0; i < tab1.length; i++) {
+		$("#tableau").append("<tr>");
+		$("#tableau").append("<td>"+tab1[i]+"</td> </tr>");
+
+	}
+
+	$("#tableau").append("</tr>");
+$("#tableau").append("</table>");
 
 
 
